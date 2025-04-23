ca995eb6e812eff100a41fa5bddee31090986e28
Ethan <ethan.frappie@gmail.com>
2025-04-23 14:20:13 +0200

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/movies", (req, res) => {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${process.env.KEYAPI}`
+        }
+      };
+
+  fetch("https://api.themoviedb.org/3/discover/movie",options)
+    .then((reponse) => reponse.json())
+    .then((data) => {
+      console.log(data);
+      res.json({movie:data.results});
+    });
+});
+
+module.exports = router;
